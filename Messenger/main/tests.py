f34175867a889b5833f6c9ae343ecdbb24836335
Bogdan <58120983+notlockeduser@users.noreply.github.com>
2020-12-15 15:34:35 +0200

    Add unitTests
    Створення декількох unit - тестів для перевірки правильності роботи реєстрації, авторизації користувача а також заповнення інформації про нього
    
    Co-Authored-By: jeniknaum5 <56956503+jeniknaum5@users.noreply.github.com>
    Co-Authored-By: Roma-x <56560165+Roma-x@users.noreply.github.com>

--- Messenger/main/tests.py
+++ Messenger/main/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from . models import UserInfo
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        UserInfo.objects.create(username="Bod", first_name="Bohdan", last_name = "Holovin", job="student", age = 9, friends=" ")
+
+    def test_first_name_label(self):
+        user = UserInfo.objects.get(id=1)
+        field_label = user._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
+
+    def test_first_name_max_length(self):
+        user = UserInfo.objects.get(id=1)
+        max_length = user._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 100)
+
+    def test_last_name_label(self):
+        user = UserInfo.objects.get(id=1)
+        field_label = user._meta.get_field('last_name').verbose_name
+        self.assertEquals(field_label, 'last name')
+
+    def test_last_name_max_length(self):
+        user = UserInfo.objects.get(id=1)
+        max_length = user._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_job(self):
+        user = UserInfo.objects.get(id=1)
+        field_label = user._meta.get_field('job').verbose_name
+        self.assertEquals(field_label, 'job')
+
+    def test_job_max_length(self):
+        user = UserInfo.objects.get(id=1)
+        max_length = user._meta.get_field('job').max_length
+        self.assertEquals(max_length, 200)
+
